6b381865e65703d729f66a344ce86d89ef799f9d
onurgonay <78919746+onurgonay@users.noreply.github.com>
2021-04-01 18:07:56 +0300

    Update mgos_esplib_test.c

--- src/mgos_esplib_test.c
+++ src/mgos_esplib_test.c
@@ -1,6 +1,14 @@
 #include "mgos.h"
 #include "mgos_esplib_test.h"
 
+
+void mgos_awake(void){
+  LOG(LL_INFO, "Mgos testlib");
+  
+  
+return;
+}
+
 bool mgos_esplib_test_init(void) {
   return true;
 }
